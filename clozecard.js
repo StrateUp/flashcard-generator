1c7df10dc1b1fb2a2f608b9e3128edef4d4684e5
Kayla Astl <kaylaastl@Kaylas-MBP.attlocal.net>
2017-04-09 16:54:14 -0700

    add this

--- clozecard.js
+++ clozecard.js
@@ -14,7 +14,6 @@ var askQuestion = function(){
 //for (count < 5) {
 
 inquirer.prompt([
-  {message: "create flashcards below"}
   {
     type: "input",
     name: "question",
@@ -43,13 +42,13 @@ inquirer.prompt([
 
 
 //testing purposes CL:  node clozecard.js
-var factA = new ClozeCard ("Penguins are flightless birds that dive for fish.","Penguins");
-var factB = new ClozeCard ("Flamingos are pink birds that get their coloration from eating shrimp.","Flamingos");
-var factC = new ClozeCard ("Boobies are pelagic member of the pelican family with blue feet and a stupid face.","Boobies");
-var factD = new ClozeCard ("Cuckoos are parasitic birds that lay their eggs in other birds nests for them to raise.","Cuckoos");
+// var factA = new ClozeCard ("Penguins are flightless birds that dive for fish.","Penguins");
+// var factB = new ClozeCard ("Flamingos are pink birds that get their coloration from eating shrimp.","Flamingos");
+// var factC = new ClozeCard ("Boobies are pelagic member of the pelican family with blue feet and a stupid face.","Boobies");
+// var factD = new ClozeCard ("Cuckoos are parasitic birds that lay their eggs in other birds nests for them to raise.","Cuckoos");
 
-console.log(factA.cloze);
-console.log(factA.fullText);
-console.log(factA.partial);
+// console.log(factA.cloze);
+// console.log(factA.fullText);
+// console.log(factA.partial);
 
 module.exports = ClozeCard
